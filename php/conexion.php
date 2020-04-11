b86d651e5fc87c906e3d690010d647115529534a
tryn0 <vkae@live.com>
2020-04-11 21:18:55 +0200

    php db

---
+++ php/conexion.php
@@ -0,0 +1,14 @@
+<?php
+    $servername = "localhost";
+    $username   = "camping2";
+    $password   = "CampingNetPI";
+    $dbname     = "camping2";
+
+    //Creacion de la conexion
+    $conn = new mysqli($servername, $username, $password, $dbname);
+    //Comprobación de la conexión
+    if ($conn->connect_error) {
+        die("La conexión ha fallado: " . $conn->connect_error);
+    }
+    //Devuelve la conexión (para ser usada)
+    return $conn;
