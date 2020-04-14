7be5cd8f6d9d87519adbe3a2e117367166922075
tryn0 <vkae@live.com>
2020-04-14 21:30:10 +0200

    conexion.php improved, added functions, close conection too

--- php/conexion.php
+++ php/conexion.php
@@ -1,14 +1,19 @@
 <?php
-    $servername = "localhost";
-    $username   = "camping2";
-    $password   = "CampingNetPI";
-    $dbname     = "camping2";
-
-    //Creacion de la conexion
-    $conn = new mysqli($servername, $username, $password, $dbname);
-    //Comprobación de la conexión
-    if ($conn->connect_error) {
-        die("La conexión ha fallado: " . $conn->connect_error);
+    function conexion(){
+        $servername = "localhost";
+        $username   = "camping2";
+        $password   = "CampingNetPI";
+        $dbname     = "camping2";
+        //Creacion de la conexion
+        $conn = new mysqli($servername, $username, $password, $dbname);
+        //Comprobación de la conexión
+        if ($conn->connect_error) {
+            die("La conexión ha fallado: " . $conn->connect_error);
+        }
+        //Devuelve la conexión (para ser usada)
+        return $conn;
     }
-    //Devuelve la conexión (para ser usada)
-    return $conn;
+
+    function cerrar_conexion($conn){
+        return $conn -> close();
+    }
