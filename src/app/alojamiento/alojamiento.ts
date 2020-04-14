efad200da94d81654032872cc26f4b52f0a6a848
tryn0 <vkae@live.com>
2020-04-14 21:25:28 +0200

    added alojamiento pages

---
+++ src/app/alojamiento/alojamiento.ts
@@ -0,0 +1,32 @@
+export class Alojamiento {
+    public idAlojamiento: number;
+    public tipo: string;
+    public sombra?: string;
+    public dimension?: string;
+    public habitaciones?: number;
+    public maximo_personas?: number;
+    public numeroAlojamiento: number;
+    
+    constructor(idAlojamiento: number, tipo: string, numeroAlojamiento: number, sombra?: string, dimension?: string, habitaciones?: number, maximo_personas?: number) {
+    this.idAlojamiento = idAlojamiento;
+    this.tipo = tipo;
+    switch(sombra) { 
+        case '0': { 
+           this.sombra = 'Nada';
+           break; 
+        } 
+        case '1': { 
+           this.sombra = 'Media';
+           break; 
+        } 
+        case '2': { 
+           this.sombra = 'Mucha';
+           break; 
+        } 
+     }
+    this.dimension = dimension;
+    this.habitaciones = habitaciones;
+    this.maximo_personas = maximo_personas;
+    this.numeroAlojamiento = numeroAlojamiento;
+    }
+}
