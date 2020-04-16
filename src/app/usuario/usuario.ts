f1e2735340c57b3ff70780fe76f2802114223eed
tryn0 <vkae@live.com>
2020-04-16 21:08:41 +0200

    usuario improved

--- src/app/usuario/usuario.ts
+++ src/app/usuario/usuario.ts
@@ -1,4 +1,5 @@
 export class Usuario {
+    public id: number;
     public email: string;
     public nif: string;
     public nombre: string;
@@ -6,12 +7,13 @@ export class Usuario {
     public telefono: number;
     public alias: string;
     
-    constructor(email: string, nif: string, nombre: string, rol: string, telefono: number, alias: string) {
-    this.email = email;
-    this.nif = nif;
-    this.nombre = nombre;
-    this.rol = rol;
-    this.telefono = telefono;
-    this.alias = alias;
+    constructor(id: number, email: string, nif: string, nombre: string, rol: string, telefono: number, alias: string) {
+        this.id = id;
+        this.email = email;
+        this.nif = nif;
+        this.nombre = nombre;
+        this.rol = rol;
+        this.telefono = telefono;
+        this.alias = alias;
     }
 }
