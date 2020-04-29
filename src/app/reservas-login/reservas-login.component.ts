d94f157737bf46f201b55d2e35d71d935968a96f
tryn0 <vkae@live.com>
2020-04-29 20:41:00 +0200

    reservas-login updated

--- src/app/reservas-login/reservas-login.component.ts
+++ src/app/reservas-login/reservas-login.component.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit, Inject } from '@angular/core';
+import { HttpClient, HttpParams } from '@angular/common/http';
+import { FormBuilder, FormGroup, Validators, FormControl, ValidationErrors } from '@angular/forms';
 import { MatDialogRef, MAT_DIALOG_DATA } from '@angular/material/dialog';
+import { Usuario } from '../usuario/usuario';
 
 interface DialogData {
   email: string;
@@ -12,12 +15,39 @@ interface DialogData {
 })
 export class ReservasLoginComponent implements OnInit {
 
-  constructor(
-    public dialogRef: MatDialogRef<ReservasLoginComponent>,
-    @Inject(MAT_DIALOG_DATA) public data: DialogData) {}
+  public comprobado: boolean = null;
 
-  onNoClick(): void {
-    this.dialogRef.close();
+  public login: FormGroup;
+
+  public existe: any;
+
+  public usuario: Usuario;
+
+  constructor(public dialogRef: MatDialogRef<ReservasLoginComponent>, @Inject(MAT_DIALOG_DATA) public data: DialogData, private fb: FormBuilder, private http: HttpClient) {
+
+    this.login = this.fb.group({
+      email: ['', [Validators.required]],
+      password: ['',  Validators.required],
+    });
+
+  }
+
+  LogIn(){
+    let fd: any = new FormData();
+    fd.append('email', this.login.get('email').value);
+    fd.append('password', this.login.get('password').value);
+
+    this.http.post<any>('http://localhost/login-usuario.php', fd).subscribe(data =>{
+      if(data != null){ // Si encuentra el usuario y la pass coincide
+        this.existe = data[0];
+        this.usuario = new Usuario(this.existe.idUsuario, this.existe.email, this.existe.nif_usuario, this.existe.nombre_usuario, this.existe.rol, this.existe.telefono, this.existe.alias_usuario);
+        localStorage.setItem('usuarioActual',JSON.stringify(this.usuario));
+        this.dialogRef.close(this.usuario);
+      }else{ // No se encuentra el usuario o la pass no coincide
+        this.comprobado = false;
+      }
+    }, error => console.log(error)
+    );
   }
 
   ngOnInit() {
