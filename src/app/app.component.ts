7be0f6622dfcb43883ef634b17795d2bd1b246f7
tryn0 <vkae@live.com>
2020-05-05 12:25:29 +0200

    home updated & improved

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,9 @@
 import { Component } from '@angular/core';
 import { MatSnackBar } from '@angular/material/snack-bar';
 import { HttpClient } from '@angular/common/http';
+import { Router, NavigationEnd } from '@angular/router';
+
+import { encriptar, desencriptar } from './crypto-storage';
 
 @Component({
   selector: 'app-root',
@@ -9,33 +12,44 @@ import { HttpClient } from '@angular/common/http';
 })
 export class AppComponent {
 
+  public dashboard: boolean = false;
+
   title = 'CampingNet';
 
-  public usuarioActual: any;
+  public usuarioActual: any = null;
 
   public mensaje: any;
 
-
   public ip: string;
 
-  constructor(private http: HttpClient, private _snackBar: MatSnackBar) {
-
-    //Mensaje de información
-    if(localStorage.getItem('mensaje') != '0'){
-      this.mensaje = this._snackBar.open('Le informamos que usamos cookies para mejorar el servicio.','No mostrar más.');
-      //Cuando se acepta el mensaje
-      this.mensaje.afterDismissed().subscribe(() => {
-        localStorage.setItem('mensaje', '0');
-      });      
-    }
+  constructor(private http: HttpClient, private _snackBar: MatSnackBar, private route: Router) {
 
     // Para obtener la IP
     this.http.get("http://api.ipify.org/?format=json").subscribe((res:any) =>{
       this.ip = res.ip;
     });
-    
-    //Comprobar si hay algún usuario con sesión iniciada
-    this.usuarioActual = JSON.parse(localStorage.getItem('usuarioActual'));
+
+    this.http.get("http://localhost/crypto.php").subscribe(data =>{
+      if(data != null){
+        let key = data as string;
+
+        //Mensaje de información
+        if(localStorage.getItem('mensaje') == null && key != null){
+          this.mensaje = this._snackBar.open('Le informamos que usamos cookies para mejorar el servicio.','No mostrar más.');
+          //Cuando se acepta el mensaje
+          this.mensaje.afterDismissed().subscribe(() => {
+            localStorage.setItem('mensaje', encriptar('0', key));
+          });      
+        }
+
+        //Comprobar si hay algún usuario con sesión iniciada
+        if(localStorage.getItem('usuarioActual') != null && key != null){
+          this.usuarioActual = desencriptar(localStorage.getItem('usuarioActual'), key);
+        }
+
+      }
+    }); 
+
   }
 
   logOff(){
@@ -43,6 +57,19 @@ export class AppComponent {
     window.location.reload();
   }
 
+
   ngOnInit(): void {  
+
+
+    this.route.events.subscribe((e) => {
+      if (e instanceof NavigationEnd) {
+        //console.log(e.url);
+        if(e.url == '/dashboard'){
+          this.dashboard = true;
+        }else{
+          this.dashboard = false;
+        }
+      }
+    });
   }
 }
