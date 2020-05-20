c7c1b41e080778f1ff5bf926d170397b0bf2f6a4
tryn0 <vkae@live.com>
2020-05-20 17:23:38 +0200

    php

--- php/resenias.php
+++ php/resenias.php
@@ -1,5 +1,5 @@
 <?php
-    //Para evitar error XMLHttpRequest en Angular
+    // Para evitar error XMLHttpRequest en Angular
     header('Access-Control-Allow-Origin: *');
     header("Access-Control-Allow-Headers: X-API-KEY, Origin, X-Requested-With, Content-Type, Accept, Access-Control-Request-Method");
     header("Access-Control-Allow-Methods: GET, POST, OPTIONS, PUT, DELETE");
@@ -9,7 +9,7 @@
         die();
     }
 
-    //Conexión a la base de datos
+    // Conexión a la base de datos
     require_once('bd.php');
     $conn = conexion();
     $opcion = json_encode($_POST['opcion']);
@@ -22,5 +22,22 @@
         $idUsuario = json_encode($_POST['idUsuario']);
         $sql = "SELECT alias_usuario FROM usuario WHERE idUsuario = $idUsuario";
         print json_encode(consulta($conn, $sql));
+    }else if($opcion == '"3"') { // Inserción de reseña
+        $idUsuario = substr(json_encode($_POST['idUsuario']),1,-1);
+        $idAlojamiento = json_encode($_POST['idAlojamiento']);
+        $comentario = json_encode($_POST['comentario']);
+        $anonimo = substr(json_encode($_POST['anonimo']),1,-1);
+        $puntuacion = substr(json_encode($_POST['puntuacion']),1,-1);
+
+        $sql = "INSERT INTO resenia (comentario, puntuacion, publicado, anonimo, idUsuario) VALUES ($comentario, $puntuacion, 0, $anonimo, $idUsuario)";
+        $resenia = insert($conn, $sql);
+
+        $conn = conexion();
+        $sql2 = "SELECT idResenia FROM resenia WHERE comentario = $comentario AND puntuacion = $puntuacion AND anonimo = $anonimo AND idUsuario = $idUsuario";
+        $idResenia = consulta($conn, $sql2)[0]['idResenia'];
+
+        $conn = conexion();
+        $sql3 = "INSERT INTO resenias (idResenia, idAlojamiento) VALUES ($idResenia, $idAlojamiento)";
+        print json_encode(insert($conn, $sql3));
     }
     
